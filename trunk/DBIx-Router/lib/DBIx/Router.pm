63663441a38393b65cda2d14c87ee28a20bcb71c
pharkins <pharkins@10af6ca5-334f-0410-b389-e59235543bd7>
2008-06-09 05:04:02 +0000

    remove inline POD, put in some stuff from null transport

--- trunk/DBIx-Router/lib/DBIx/Router.pm
+++ trunk/DBIx-Router/lib/DBIx/Router.pm
@@ -3,6 +3,60 @@ package DBIx::Router;
 use warnings;
 use strict;
 
+use base qw(DBD::Gofer::Transport::Base);
+
+use Carp;
+use DBI 1.55;
+use DBI::Gofer::Execute;
+use Config::Any;
+
+our $VERSION = '0.01';
+
+__PACKAGE__->mk_accessors(
+    qw(
+      pending_response
+      transmit_count
+      )
+);
+
+my $executor = DBI::Gofer::Execute->new();
+
+sub transmit_request_by_transport {
+    my ( $self, $request ) = @_;
+    $self->transmit_count( ( $self->transmit_count() || 0 ) + 1 )
+      ;    # just for tests
+
+    my $frozen_request = $self->freeze_request($request);
+
+    # ...
+    # magic routing happens here
+    # ...
+
+    my $response =
+      $executor->execute_request(
+        $self->thaw_request( $frozen_request, undef, 1 ) );
+
+    # put response 'on the shelf' ready for receive_response()
+    $self->pending_response($response);
+
+    return undef;
+}
+
+# Do we even need this really? Http transport doesn't use it.
+sub receive_response_by_transport {
+    my $self = shift;
+
+    my $response = $self->pending_response;
+
+    my $frozen_response = $self->freeze_response( $response, undef, 1 );
+
+    return $self->thaw_response($frozen_response);
+}
+
+1;
+
+__END__
+
 =head1 NAME
 
 DBIx::Router - The great new DBIx::Router!
@@ -10,11 +64,6 @@ DBIx::Router - The great new DBIx::Router!
 =head1 VERSION
 
 Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
@@ -36,17 +85,7 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head2 function1
 
-=cut
-
-sub function1 {
-}
-
 =head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
@@ -58,15 +97,11 @@ Please report any bugs or feature requests to C<bug-dbix-router at rt.cpan.org>,
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Router>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc DBIx::Router
-
 
 You can also look for information at:
 
@@ -90,9 +125,7 @@ L<http://search.cpan.org/dist/DBIx-Router>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
-
 
 =head1 COPYRIGHT & LICENSE
 
@@ -101,7 +134,4 @@ Copyright 2008 Perrin Harkins, all rights reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
-
-1; # End of DBIx::Router
