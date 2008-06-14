1bf3163e16eda2c40dc961bccaa0e3e239e151d7
pharkins <pharkins@10af6ca5-334f-0410-b389-e59235543bd7>
2008-06-14 08:11:58 +0000

    some class name changes and some progress on defining basic structure of routing

---
+++ DBIx-Router/t/conf.pl
@@ -0,0 +1,28 @@
+{
+    datasources => [
+        {
+            name     => Test1,
+            dsn      => 'dbi:DBM:dbm_type=SDBM_File;lockfile=0',
+            user     => undef,
+            password => undef,
+        },
+        {
+            name     => Test2,
+            dsn      => 'dbi:DBM:dbm_type=SDBM_File;lockfile=0',
+            user     => undef,
+            password => undef,
+        },
+
+    ],
+    rules => [
+
+        #         {
+        #             class      => readonly,
+        #             datasource => Test1,
+        #         },
+        {
+            class      => default,
+            datasource => Test2,
+        }
+    ],
+}
