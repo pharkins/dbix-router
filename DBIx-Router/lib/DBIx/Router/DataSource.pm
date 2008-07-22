042cb2d94f841d157d3c3d5df3a90e0c066e9bc4
pharkins <pharkins@10af6ca5-334f-0410-b389-e59235543bd7>
2008-07-22 19:55:12 +0000

    fix some docs

--- DBIx-Router/lib/DBIx/Router/DataSource.pm
+++ DBIx-Router/lib/DBIx/Router/DataSource.pm
@@ -22,103 +22,22 @@ __END__
 
 =head1 NAME
 
-DBIx::Router::DataSource - The great new DBIx::Router::DataSource!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+DBIx::Router::DataSource - Base class for data sources
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This is a base class that all DataSource classes must inherit from.
 
-Perhaps a little code snippet.
+    use base qw(DBIx::Router::DataSource);
+    
+    $self->name('my_data_source');
 
-    use DBIx::Router::DataSource;
+=head1 METHODS
 
-    my $foo = DBIx::Router::DataSource->new();
-    ...
+=head2 name
 
-=head1 EXPORT
+Accessor for name attribute.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head2 execute_request($request, $router)
 
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Perrin Harkins, C<< <perrin at elem.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-dbix-router-datasource at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Router>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc DBIx::Router
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-Router>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DBIx-Router>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DBIx-Router>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DBIx-Router>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Perrin Harkins, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of DBIx::Router::DataSource
+This is a virtual method that all subclasses must implement.  It is passed a request which it then executes.  The second parameter is a reference to the DBIx::Router instance, used to get the current conf if the DataSource class wants to check config parameters.
--- DBIx-Router/lib/DBIx/Router/RuleList.pm
+++ DBIx-Router/lib/DBIx/Router/RuleList.pm
@@ -35,103 +35,24 @@ __END__
 
 =head1 NAME
 
-DBIx::Router::RuleList - The great new DBIx::Router::RuleList!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+DBIx::Router::RuleList - Apply a list of Rules in a specified order
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+    my $rule_list = DBIx::Router::RuleList->new( { rules => \@rules } );
+    
+    my $datasource = $rule_list->map_request($request);
 
-Perhaps a little code snippet.
+=head1 METHODS
 
-    use DBIx::Router::RuleList;
+=head2 new({ rules => \@rules })
 
-    my $foo = DBIx::Router::RuleList->new();
-    ...
+Constructor, requires rules parameter which is an arrayref of Rule objects.
 
-=head1 EXPORT
+=head2 rules
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Accessor for rules arrayref.
 
-=head1 FUNCTIONS
+=head2 map_request($request)
 
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Perrin Harkins, C<< <perrin at elem.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-dbix-router-rulelist at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Router>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc DBIx::Router
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-Router>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DBIx-Router>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DBIx-Router>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DBIx-Router>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Perrin Harkins, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of DBIx::Router::RuleList
+This is passed a request and tries each Rule in order until it finds one that will accept the request.  When it finds one, it returns that Rule's datasource.  If none is found, it will throw an exception.  Normally a RuleList would either have a default rule at the end or use the C<passthrough> option.
