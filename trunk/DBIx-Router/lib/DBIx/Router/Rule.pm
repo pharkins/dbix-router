941919a4619e1f345908a6c6f21bfa4881bc6c03
pharkins <pharkins@10af6ca5-334f-0410-b389-e59235543bd7>
2008-07-22 19:55:12 +0000

    fix some docs

--- trunk/DBIx-Router/lib/DBIx/Router/Rule.pm
+++ trunk/DBIx-Router/lib/DBIx/Router/Rule.pm
@@ -28,103 +28,26 @@ __END__
 
 =head1 NAME
 
-DBIx::Router::Rule - The great new DBIx::Router::Rule!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+DBIx::Router::Rule - Base class for rules
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This is a base class that all Rule classes must inherit from.
 
-Perhaps a little code snippet.
+    use base qw(DBIx::Router::Rule);
+    
+    my $datasource_obj = $self->datasource;
 
-    use DBIx::Router::Rule;
+=head1 METHODS
 
-    my $foo = DBIx::Router::Rule->new();
-    ...
+=head2 new({ datasource => $datasource_obj })
 
-=head1 EXPORT
+Constructor, requires datasource parameter.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head2 datasource
 
-=head1 FUNCTIONS
+Accessor for DataSource object.
 
-=head2 function1
+=head2 accept
 
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Perrin Harkins, C<< <perrin at elem.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-dbix-router-rule at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Router>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc DBIx::Router
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-Router>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DBIx-Router>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DBIx-Router>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DBIx-Router>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Perrin Harkins, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of DBIx::Router::Rule
+This is a virtual method that all subclasses must implement.  It is passed a request and determines whether or not the request matches this rule, returning true or false.
